df89bc2792edf3d7dd87aea54d6ba57120526fb0
Jeferson Fernando <JefersonFrnando>
2022-06-26 19:57:54 -0300

    Utilizar timers para controlar estados

--- SemaphoreArduino/SemaphoreArduino/main.asm
+++ SemaphoreArduino/SemaphoreArduino/main.asm
@@ -1,42 +1,91 @@
-; Program 8.1 ? LED Blinker
-; Illustrate the use of a Timer/Counter to blink an LED
-; LEDBlinker_Timer.asm
+; Projeto de semáforo com Arduino utilizando chaveamento de LEDs.
 ;
-; Created: 27/02/2017 11:56:54
-; Author : Erick
+; Created: 26/06/2022
+; Author : Itallo Patrick
+; Author : Jeferson Fernando
+; Author : João Victor Holanda
+; Author : Lucas Buarque 
 ;
-;LED's on PORTB
 ;Clock speed 16 MHz
 
-;Timer 1 é utilizado para definir um intervalo de 0,5 s
-;A cada intervalo os LEDs piscam
-;4 LEDs conectados a PORTB
+.cseg
+#define RED_LED_BIT 2
+#define YELLOW_LED_BIT 1
+#define GREEN_LED_BIT 0
+
+#define MAX_STATE 7
+
+#define CLOCK 16.0e6 ;clock speed
+#define TIMER_1_DELAY 1.0e-3 ;seconds
+#define TIMER_2_DELAY  4.0e-3
 
 .def temp = r16
 .def tpointer = r17 ;current LED value
-.cseg
-timers:
-.db $10, $20, $30, $40, $50, $60, $70, $80
-
-ldi ZH, high(timers*2)
-ldi ZL, low(timers*2)
+.def current_state = r18
+.def remaining_state_time = r19
+.def current_showing = r20
 
 jmp reset
+
+.org OC2Aaddr
+jmp OCI2A_Interrupt
 .org OC1Aaddr
 jmp OCI1A_Interrupt
 
-OCI1A_Interrupt:
+OCI2A_Interrupt:
 	push r16
 	in r16, SREG
 	push r16
-	
-	lpm r17, Z+
 	
 	pop r16
 	out SREG, r16
 	pop r16
 	reti
 
+OCI1A_Interrupt:
+	push r16
+	in r16, SREG
+	push r16
+
+	dec remaining_state_time
+
+	tst remaining_state_time
+	brne return_int1 
+	
+	inc current_state
+	cpi current_state, MAX_STATE
+	brne update_state_time
+
+	ldi current_state, 0
+	ldi ZH, high(STATE_TIME*2)
+	ldi	ZL, low(STATE_TIME*2)
+
+	update_state_time:
+	lpm remaining_state_time, Z+
+
+	return_int1:
+		pop r16
+		out SREG, r16
+		pop r16
+		reti
+
+STATE_TIME:
+.db 26, 4, 56, 4, 11, 4, 20
+
+SEMAPHORE_ONE_STATE:
+.db 0b001, 0b001, 0b001, 0b010, 0b100, 0b100, 0b100
+
+SEMAPHORE_TWO_STATE:
+.db 0b001, 0b010, 0b100, 0b100, 0b100, 0b100, 0b100
+
+SEMAPHORE_THREE_STATE:
+.db 0b100, 0b100, 0b001, 0b010, 0b100, 0b100, 0b100
+
+SEMAPHORE_FOUR_STATE:
+.db 0b100, 0b100, 0b100, 0b100, 0b001, 0b010, 0b100
+
+SEMAPHORE_PEDESTRIAN_STATE:
+.db 0b100, 0b100, 0b100, 0b100, 0b100, 0b100, 0b001
 
 reset:
 	;Stack initialization
@@ -44,38 +93,61 @@ reset:
 	out SPL, temp
 	ldi temp, high(RAMEND)
 	out SPH, temp
-	
-	ldi tpointer, 0
-	#define CLOCK 16.0e6 ;clock speed
-	#define DELAY 1.0e-3 ;seconds
+
+	ldi current_state, 0
+	ldi ZH, high(STATE_TIME*2)
+	ldi	ZL, low(STATE_TIME*2)
+	lpm remaining_state_time, Z+
+
+
+	.equ WGM = 0b0100 ;Waveform generation mode: CTC
 	.equ PRESCALE = 0b100 ;/256 prescale
 	.equ PRESCALE_DIV = 256
-	.equ WGM = 0b0100 ;Waveform generation mode: CTC
+	
 	;you must ensure this value is between 0 and 65535
-	.equ TOP = int(0.5 + ((CLOCK/PRESCALE_DIV)*DELAY))
+	.equ TOP = int(0.5 + ((CLOCK/PRESCALE_DIV)*TIMER_1_DELAY))
 	.if TOP > 65535
 	.error "TOP is out of range"
 	.endif
 
-	;On MEGA series, write high byte of 16-bit timer registers first
+	.equ TOP_TWO = int(0.5 + ((CLOCK/PRESCALE_DIV)*TIMER_2_DELAY))
+	.if TOP_TWO > 65535
+	.error "TOP is out of range"
+	.endif
+
+	///// TOP UM /////
 	ldi temp, high(TOP) ;initialize compare value (TOP)
 	sts OCR1AH, temp
 	ldi temp, low(TOP)
 	sts OCR1AL, temp
+
 	ldi temp, ((WGM&0b11) << WGM10) ;lower 2 bits of WGM
 	; WGM&0b11 = 0b0100 & 0b0011 = 0b0000 
 	sts TCCR1A, temp
 	;upper 2 bits of WGM and clock select
 	ldi temp, ((WGM>> 2) << WGM12)|(PRESCALE << CS10)
-	; WGM >> 2 = 0b0100 >> 2 = 0b0001
-	; (WGM >> 2) << WGM12 = (0b0001 << 3) = 0b0001000
-	; (PRESCALE << CS10) = 0b100 << 0 = 0b100
-	; 0b0001000 | 0b100 = 0b0001100
-	sts TCCR1B, temp ;start counter
+	sts TCCR1B, temp
 
 	lds r16, TIMSK1
 	sbr r16, 1 <<OCIE1A
 	sts TIMSK1, r16
+
+	///// TOP DOIS /////
+	ldi temp, TOP_TWO ;initialize compare value (TOP)
+	sts OCR2A, temp
+
+	;ldi temp, ((WGM&0b11) << WGM10) ;lower 2 bits of WGM
+	ldi temp, 0b00000010
+	; WGM&0b11 = 0b0100 & 0b0011 = 0b0000 
+	sts TCCR2A, temp
+
+	ldi temp, 0b00000110
+	; WGM&0b11 = 0b0100 & 0b0011 = 0b0000 
+	sts TCCR2B, temp
+
+	lds r16, TIMSK2
+	sbr r16, 1 <<OCIE2A
+	sts TIMSK2, r16
 
 	sei
 	main_lp:
